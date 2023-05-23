623af187b5a842db8e46899496440c1ffb1df07f
luosi <568837624@qq.com>
2023-05-23 17:00:35 +0800

    revert

--- services/nsfw/project/__init__.py
+++ services/nsfw/project/__init__.py
@@ -6,24 +6,11 @@ from flask import (
     send_from_directory,
     request,
 )
-from flask_sqlalchemy import SQLAlchemy
 from werkzeug.utils import secure_filename
 from nsfw_detector import predict
 
 app = Flask(__name__)
 app.config.from_object("project.config.Config")
-db = SQLAlchemy(app)
-
-
-class User(db.Model):
-    __tablename__ = "users"
-
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(128), unique=True, nullable=False)
-    active = db.Column(db.Boolean(), default=True, nullable=False)
-
-    def __init__(self, email):
-        self.email = email
 
 
 @app.route("/")

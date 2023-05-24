5301611e40cc28f0decc2dc3f88bbf7b746821b9
luosi <568837624@qq.com>
2023-05-24 11:38:42 +0800

    nsfw

--- services/nsfw/project/__init__.py
+++ services/nsfw/project/__init__.py
@@ -40,6 +40,17 @@ def scan_ai():
         model = predict.load_model(model_path)
         # Predict single image
         return predict.classify(model, fp)
+# 其他示例
+# {'2.jpg': {'sexy': 4.3454722e-05, 'neutral': 0.00026579265, 'porn': 0.0007733492, 'hentai': 0.14751932, 'drawings': 0.85139805}}
+
+# Predict multiple images at once
+#predict.classify(model, ['/Users/bedapudi/Desktop/2.jpg', '/Users/bedapudi/Desktop/6.jpg'])
+# {'2.jpg': {'sexy': 4.3454795e-05, 'neutral': 0.00026579312, 'porn': 0.0007733498, 'hentai': 0.14751942, 'drawings': 0.8513979}, '6.jpg': {'drawings': 0.004214506, 'hentai': 0.013342537, 'neutral': 0.01834045, 'porn': 0.4431829, 'sexy': 0.5209196}}
+
+# Predict for all images in a directory
+#predict.classify(model, '/Users/bedapudi/Desktop/')
+#
+
 
     # return """
     # <!doctype html>

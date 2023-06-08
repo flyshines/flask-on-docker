59872a9f067c2190f1ea87a3e5307b704fe6e661
luosi <568837624@qq.com>
2023-06-08 15:30:38 +0800

    memory fix

--- services/nsfw/manage.py
+++ services/nsfw/manage.py
@@ -6,4 +6,5 @@ from project import app
 # cli = FlaskGroup(app)
 
 if __name__ == "__main__":
-    app.run(host='127.0.0.1', port=5002)
+    # debug=False threaded=False 单线程跑，否则会有内存泄漏问题，还没有好的解决方案
+    app.run(host='127.0.0.1', port=5002, debug=False, threaded=False)

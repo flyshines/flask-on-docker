9da702646df0257136b53c7dbf9f9aeb9aa7eee8
luosi <luosi@zhuanzhuan.com>
2023-08-23 07:51:26 +0800

    thread

--- services/nsfw/manage.py
+++ services/nsfw/manage.py
@@ -7,4 +7,4 @@ from project import app
 
 if __name__ == "__main__":
     # debug=False threaded=False 单线程跑，否则会有内存泄漏问题，还没有好的解决方案
-    app.run(host='127.0.0.1', port=5002, debug=False, threaded=False)
+    app.run(host='127.0.0.1', port=5002, debug=False)
